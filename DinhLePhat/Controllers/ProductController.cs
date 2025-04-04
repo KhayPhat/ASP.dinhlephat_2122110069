39d886dcb0114da603ca82fdcffe05124f994838
ASP.dinhlephat_2122110069 <dinhlephatdanthuong@gmail.com>
2025-04-04 13:38:28 +0700

    asp

---
+++ DinhLePhat/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services;
+using DinhLePhat.Context;
+
+namespace DinhLePhat.Controllers
+{
+    public class ProductController : Controller
+    {
+        Website_LAEntities objWebsite_LAEntities = new Website_LAEntities();
+
+        // GET: ProductWebsite_LAEntities
+        public ActionResult Index()
+        {
+            var lstProduct = objWebsite_LAEntities.Products.ToList();
+            return View(lstProduct);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product objPro)
+        {
+            objWebsite_LAEntities.Products.Add(objPro);
+            objWebsite_LAEntities.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
